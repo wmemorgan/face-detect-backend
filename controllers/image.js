f59cb8f5e1217f6b3e7a45c17cfaf766a421b267
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-02-22 18:09:06 +0800

    Upload project to GitHub

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'b8db7f238baf4f64a53fa01d14489584'
+});
+
+const handleApiCall = (req, res) => { 
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('Unable to retrieve from API.'))
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('Unable to get entries.'))
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}
