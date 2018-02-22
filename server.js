be2ff25a5330d50b3dd6f4d18cf7680133f153b7
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-02-22 21:01:26 +0800

    Change database address

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-rectangular-24667',
     user: 'wmemorgan',
-    password: 'data1234',
+    password: '',
     database: 'smart-brain'
   }
 });
